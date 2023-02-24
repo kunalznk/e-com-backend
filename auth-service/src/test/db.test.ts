b6535cfc422517cd48226f553fa614e6bb364533
kunal.zanke <kunal.zanke@globallogic.com>
2023-02-24 19:32:47 +0530

    added circle ci config

--- auth-service/src/test/db.test.ts
+++
@@ -1,8 +0,0 @@
-import {describe, expect, test} from '@jest/globals';
-import sequelize from "../config/db"
-
-describe("DB Setup Test" , () => {
-    test("Connection Test" , async () => {
-        await expect(sequelize.validate()).resolves.toBe(undefined)
-    });
-})
